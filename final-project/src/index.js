9bb6b1fbaf8830fa1fde549347adf1937b741099
Rohan Khemani <rohank2894@gmail.com>
2019-05-11 23:32:14 -0400

    Added Firebase part alongwith some routes

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -5,12 +5,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import Header from './components/header';
-ReactDOM.render((        
-        <BrowserRouter>    
-            <Header>
-                <App />
-            </Header>
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+ReactDOM.render((
+    <div>
+        <FirebaseContext.Provider value={new Firebase()}>
+        <BrowserRouter>
+            <App />
         </BrowserRouter>
+    </FirebaseContext.Provider>
+    </div>
 ), document.getElementById('root'));
 registerServiceWorker();

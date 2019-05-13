bd465aee5a97b1fd4adb006463bbbe4052c80ace
ravid7 <rrathor2@stevens.edu>
2019-05-12 20:24:57 -0400

    header removed from signin/up

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -44,34 +44,6 @@ class App extends Component {
 
         return (
             <div className="window">
-                <header className="toolbar toolbar-header">
-                    <div className="toolbar-actions">
-                        <div className="btn-group">
-                            <button className="btn btn-default">
-                                <span className="icon icon-left-open"></span>
-                            </button>
-                            <button className="btn btn-default">
-                                <span className="icon icon-right-open"></span>
-                            </button>
-                            <button className="btn btn-default">
-                                <span className="icon icon-dot-3"></span>
-                            </button>
-                        </div>
-                        <button className="btn btn-default">
-                            <span className="icon icon-home icon-text"></span>
-                            Home
-                </button>
-                        <button className="btn btn-default">
-                            <span className="icon icon-newspaper icon-text"></span>
-                            Documents
-                </button>
-                        <button className="btn btn-default">
-                            <span className="icon icon-chat icon-text"></span>
-                            Chatroom
-                </button>
-                    </div>
-                </header>
-
                 <Switch>
                     <Route exact path={ROUTES.LANDING} component={Landing} />
                     <Route exact path="/signin" component={SignInPage} />
